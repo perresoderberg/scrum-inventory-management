c7a5caa34a0bdec3a36a58b65ec0f1520ed96e04
Hannes Lundin <hanneslundin@hotmail.com>
2026-08-06 14:07:09 +0200

    Added all statistic boxes

--- components/InventoryStats.tsx
+++ components/InventoryStats.tsx
@@ -1,4 +1,4 @@
-
+import styles from "./InventoryStats.module.css";
 
 export default function InventoryStats(){//{ products }: { products: ProductsResponse["products"] }) {
   const totalProducts = 193;// products.length;
@@ -7,27 +7,34 @@ export default function InventoryStats(){//{ products }: { products: ProductsRes
   const outOfStockProducts = 4;// products.filter((product) => product.stock === 0).length;
   //const totalItems = 145;// products.reduce((sum, product) => sum + (product.stock || 0), 0);
   return (
-    <article style={{ display: "inline-flex", gap: "1rem", marginBottom: "1rem" }}>
-      <section style={{ display: "inline-flex", justifyContent: "space-between", minWidth: "300px", fontSize: "16px", border: "1px solid #ccc", padding: "1rem", marginBottom: "1rem", backgroundColor: "#f1f1f1", borderRadius: "8px" }}>
-        <div style={{ fontSize: "12px", color: "#757575", fontWeight: "600" }}>PRODUCTS
-            <p style={{ fontSize: "24px", fontWeight: "bold", color: "#7769ca" }}>{totalProducts}</p>
+    <article className={styles.inventorystatsArticle}>
+      <section className={styles.inventorystatsCard}>
+        <div>
+          <div style={{ fontSize: "12px", color: "#757575", fontWeight: "600" }}>TOTAL ITEMS</div>
+          <div style={{ fontSize: "24px", fontWeight: "bold", color: "#7769ca" }}>{totalProducts}</div>
         </div>
-        <img src="/icons/product.svg" alt="Product Icon" width={30} height={30} /> {/* Adjust the path to your icon */}
+        <img src="/icons/product.svg" alt="Total Items Icon" width={30} height={30} /> {/* Adjust the path to your icon */}
       </section>
-      <section style={{ display: "inline-flex", justifyContent: "space-between", minWidth: "300px", fontSize: "16px", border: "1px solid #ccc", padding: "1rem", marginBottom: "1rem", backgroundColor: "#f1f1f1", borderRadius: "8px" }}>
-        <div style={{ fontSize: "12px", color: "#757575", fontWeight: "600" }}>IN STOCK</div>
-        <p>{inStockProducts}</p>
+      <section className={styles.inventorystatsCard}>
+        <div>
+            <div style={{ fontSize: "12px", color: "#757575", fontWeight: "600" }}>IN STOCK</div>
+            <div style={{ fontSize: "24px", fontWeight: "bold", color: "#23a72d" }}>{inStockProducts}</div>
+        </div>
         <img src="/icons/check-circle.svg" alt="In Stock Icon" width={30} height={30} /> {/* Adjust the path to your icon */}
       </section>
-      <section style={{ display: "inline-flex", justifyContent: "space-between", minWidth: "300px", fontSize: "16px", border: "1px solid #ccc", padding: "1rem", marginBottom: "1rem", backgroundColor: "#f1f1f1", borderRadius: "8px" }}>
-        <div style={{ fontSize: "12px", color: "#757575", fontWeight: "600" }}>LOW STOCK</div>
-        <p>{lowStockProducts}</p>
+      <section className={styles.inventorystatsCard}>
+        <div>
+            <div style={{ fontSize: "12px", color: "#757575", fontWeight: "600" }}>LOW STOCK</div>
+            <div style={{ fontSize: "24px", fontWeight: "bold", color: "#e79b3c" }}>{lowStockProducts}</div>
+        </div>
         <img src="/icons/warning.svg" alt="Low Stock Icon" width={30} height={30} /> {/* Adjust the path to your icon */}
       </section>
-      <section style={{ display: "inline-flex", justifyContent: "space-between", minWidth: "300px", fontSize: "16px", border: "1px solid #ccc", padding: "1rem", marginBottom: "1rem", backgroundColor: "#f1f1f1", borderRadius: "8px" }}>
-        <div style={{ fontSize: "12px", color: "#757575", fontWeight: "600" }}>OUT OF STOCK</div>
-          <p>{outOfStockProducts}</p>
-          <img src="/icons/cancel.svg" alt="Out of Stock Icon" width={30} height={30  } /> {/* Adjust the path to your icon */}
+      <section className={styles.inventorystatsCard}>
+        <div>
+            <div style={{ fontSize: "12px", color: "#757575", fontWeight: "600" }}>OUT OF STOCK</div>
+            <div style={{ fontSize: "24px", fontWeight: "bold", color: "#dc4d32" }}>{outOfStockProducts}</div>
+        </div>
+        <img src="/icons/cancel.svg" alt="Out of Stock Icon" width={30} height={30  } /> {/* Adjust the path to your icon */}
       </section>
     </article>
   );
