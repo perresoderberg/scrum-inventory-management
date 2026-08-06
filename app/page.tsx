545b269cacf3970da5d792193c1af4d73cfb4dae
Per Söderberg <per.soderberg@gmail.com>
2026-08-06 12:19:13 +0200

    Add pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import ProductTable from "@/components/ProductTable";
+import Pagination from "@/components/Pagination";
 import { getProducts } from "@/services/product-api";
 import InventoryHeader from "../components/InventoryHeader";
 
@@ -6,12 +7,13 @@ export default async function Home() {
   const resp = await getProducts();
   console.log(resp);
 
-  const { products } = await getProducts();
+  const { products, total, page } = await getProducts();
 
   return (
     <main>
       <InventoryHeader />
       <ProductTable products={products} />
+      <Pagination currentPage={page} totalPages={total} />
     </main>
   );
 }
