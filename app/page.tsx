9cb270420c8210a243d088836f11eb8993686a53
Per Söderberg <per.soderberg@gmail.com>
2026-08-27 12:49:56 +0200

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default async function Home({ searchParams }: HomeProps) {
     ]);
 
   return (
-    <main className="mx-auto">
+    <main className="mx-auto bg-[#F5F5F5] w-400">
       <InventoryHeader />
       {/* <AddProductForm /> */}
       <InventoryStats products={allProducts} />
@@ -60,7 +60,6 @@ export default async function Home({ searchParams }: HomeProps) {
         stock={stock}
       />
       <Pagination currentPage={page} totalPages={pages} />
-
     </main>
   );
 }
